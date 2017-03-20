575a4bfe3be48c88e0eb58b576fbc3a44be69635
zeziba <zeziba@mail.com>
2017-03-20 16:06:33 -0600

    Update Beacon_Code.ino
    Updated

--- Beacon_Code.ino
+++ Beacon_Code.ino
@@ -1,6 +1,6 @@
 /*
  * Created By: Charles Engen
- * Modified Friday March 17, 2017
+ * Modified Friday March 20, 2017
  * 
  * Built for Colorado Space Grant Consortium some code extracts were used from https://github.com/Colorado-Space-Grant-Consortium/Robotics_Challenge/blob/2016_master/2017_Robotics_Challenge/xb_Transmitter_System_Code/xb_TX_Combo/xb_TX_Combo.ino
  * to match the system used
@@ -35,7 +35,9 @@
 #include <LiquidCrystal.h>
 
 // Adress location for magnetometer
-#define Mag 0x1E
+// The magnometer can only communicate at 160hz max, this translates into a 0.00625 second delay between each signal
+// This translates into a max of 160 readings per second
+#define Mag 0x1E 
 
 // PI constant
 #define M_PI 3.14159265358979311599796346854418516159057617188
@@ -70,12 +72,30 @@ union{
 
 // Below Code allows the beacon to output the systems RPM allowing the user to calibrate the speed of rotation.
 
+boolean cont = true;  // Use to turn off and on the continuous read mode
+typedef enum
+{
+    DATARATE_75HZ       = 0b110,
+    DATARATE_30HZ       = 0b101,
+    DATARATE_15HZ       = 0b100,
+    DATARATE_7_5HZ      = 0b011,
+    DATARATE_3HZ        = 0b010,
+    DATARATE_1_5HZ      = 0b001,
+    DATARATE_0_75_HZ    = 0b000
+} dataRates;  // values taken from http://www.jameco.com/Jameco/Products/ProdDS/2150248.pdf
+int hz = 75; //match the switch for hz range
+dataRates rate;
+
 float lastRead, lastWriteRead; // Time for getting data from the compass
 unsigned long compassError;
-int mps = 45; // Max RPM of 55-65 or this will now work, mps is the measure per sec
+int mps = 100; // Max RPM of 55-65 or this will now work, mps is the measure per sec
 long rpm_t;
 double rpm_m;
 boolean rpm_s;
+
+int calibration_cycle = 3; // Value is in minutes
+long calibration_time;
+int calibration_cycles = 240;
 
 // End of code to check antenna
 
@@ -89,6 +109,34 @@ void setup() {
   
   Serial.println(F("Initializing sub-systems"));
 
+  if (cont)
+    switch(hz)
+    {
+      case 75:
+        rate = DATARATE_75HZ;
+        break;
+      case 30:
+        rate = DATARATE_30HZ;
+        break;
+      case 15:
+        rate = DATARATE_15HZ;
+        break;
+      case 57:
+        rate = DATARATE_7_5HZ;
+        break;
+      case 3:
+        rate = DATARATE_3HZ;
+        break;
+      case 51:
+        rate = DATARATE_1_5HZ;
+        break;
+      case 750:
+        rate = DATARATE_0_75_HZ;
+        break;
+      default:
+        rate = DATARATE_15HZ;
+    }
+
   lcd.begin(20, 4);
   lcd.print(F("Starting..."));
   lcd.noAutoscroll();
@@ -97,15 +145,39 @@ void setup() {
   xBEE.begin(57600);
   xBee.setSerial(xBEE);
 
-  // Start Magnetometer
-  Wire.beginTransmission(Mag);
-  // Tells magnetometer to enable writing
-  Wire.write(0x02);
-  // tells mag to go to measure mode
-  Wire.write(0x00);
-  Wire.endTransmission();
+  if (cont)
+  {
+    // Start Magnetometer
+    Wire.beginTransmission(Mag);
+    // Tells magnetometer to enable writing
+    Wire.write(0x02);
+    // tells mag to go to measure mode
+    Wire.write(0x00);
+    Wire.endTransmission();
 
-  mps = (1000 - (compassError * mps)) / mps;
+    // Change data rate to 75 hz
+    Wire.beginTransmission(Mag);
+    Wire.write(0x00);
+    Wire.endTransmission();
+
+    uint8_t value = 0;
+    
+    Wire.beginTransmission(Mag);
+    Wire.requestFrom(Mag, 1);
+    while(!Wire.available())
+      value = Wire.read();
+    Wire.endTransmission();
+    value &= 0b11100011;
+    value |= (rate << 2);
+
+    Wire.beginTransmission(Mag);
+    Wire.write(0x00);
+    Wire.write(value);
+    Wire.endTransmission();
+
+    mps = hz;
+    // End of change rate change
+  }
 
   delay(1000);
 
@@ -115,6 +187,10 @@ void setup() {
   lastRead = lastRun;
   lastSend = lastRead;
   rpm_t = lastSend;
+
+  calibration();
+
+  delay(1000);
   
   lcd.clear();
   lcd.begin(20, 4);
@@ -149,9 +225,9 @@ void loop() {
     lcd.setCursor(0,0);
     lcd.print((String)F("Heading: ") + (String)heading_converter.f + char(223));
     lcd.setCursor(0,1);
-    lcd.print((String)F("Error: ") + (String)(compassError) + " ms");
+    lcd.print((String)F("Error: ") + (String)(compassError) + " ms " + F("MPS: ") + mps);
     lcd.setCursor(0,2);
-    lcd.print((String)F("RPM: ") + RPM);
+    lcd.print((String)F("RPM: ") + RPM + F(" HZ: ") + (RPM / 60));
     lcd.setCursor(0,3);
     lcd.print((String)F("Paylod: ") + (String)payload[0] + (String)payload[1] + (String)payload[2] + (String)payload[3]);
     lastRun = millis();
@@ -181,6 +257,12 @@ void loop() {
     RPM = calculate_RPM(rpm_m, heading_converter.f, (double)abs(millis() - rpm_t));
     rpm_s = false;
   }
+
+  if (time_ - calibration_time > (calibration_cycle * 60000))
+  {
+    calibration();
+    calibration_time = time_;
+  }
 }
 /*--------------------------------------------------------------
 This the the function which gathers the heading from the compass.
@@ -195,6 +277,8 @@ void getVector () {
   // but i2c adressing uses the high 7 bits so it's 33 
   Wire.write(0x03);          // command sensor to measure angle  
   Wire.endTransmission();  // stop transmitting 
+
+  // The above code is not required when in continuous mode as the sensor only updates at its predefined rate
 
   // step 3: request reading from sensor 
   Wire.requestFrom(Mag, 6);
@@ -217,7 +301,8 @@ void getVector () {
   x *= xscale;
   y *= yscale;
   z *= zscale;
-  
+
+  // Ensure the compass is level for this to work.
   float heading = atan2(y,x);
   heading += PI/2;
   if(heading < 0)
@@ -238,3 +323,56 @@ double calculate_RPM(double aOne, double aTwo, double aTime)
   return abs(aOne - aTwo) * (1000 / (aTime * 6));
 }
 
+
+void calibration()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(F("Starting Calibration."));
+  int x, y, z;
+  int minX = 0;
+  int maxX = 0;
+  int minY = 0;
+  int maxY = 0;
+  int offX = 0;
+  int offY = 0;
+
+  for (int i = 0; i < calibration_cycles; i++)
+  {
+      // step 1: instruct sensor to read echoes 
+    Wire.beginTransmission(Mag);  // transmit to device
+    // the address specified in the datasheet is 66 (0x42) 
+    // but i2c adressing uses the high 7 bits so it's 33 
+    Wire.write(0x03);          // command sensor to measure angle  
+    Wire.endTransmission();  // stop transmitting 
+  
+    // The above code is not required when in continuous mode as the sensor only updates at its predefined rate
+  
+    // step 3: request reading from sensor 
+    Wire.requestFrom(Mag, 6);
+    if(6 <= Wire.available())     // if two bytes were received 
+    { 
+      x = Wire.read()<<8; //X msb
+      x |= Wire.read(); //X lsb
+      z = Wire.read()<<8; //Z msb
+      z |= Wire.read(); //Z lsb
+      y = Wire.read()<<8; //Y msb/
+      y |= Wire.read(); //Y lsb
+    }
+  
+    if (x < minX) minX = x;
+    if (x > maxX) maxX = x;
+    if (y < minY) minY = y;
+    if (y > maxY) maxY = y;
+  
+    offX = (maxX + minX) / 2;
+    offY = (maxY + minY) / 2;
+    delay(1000 / mps);
+  }
+
+  xoff = offX;
+  yoff = offY;
+
+  lcd.clear();
+  lcd.print(F("Finished Calibration"));
+}
